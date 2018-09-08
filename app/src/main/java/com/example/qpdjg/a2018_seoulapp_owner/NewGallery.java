c5dc16a4e94906479b5e5f34adc81943bea7e6a9
JeonHyungJin <qpdjgjsl@gmail.com>
2018-09-08 17:53:44 +0900

    Storage와 DB에 사진 업로드

--- app/src/main/java/com/example/qpdjg/a2018_seoulapp_owner/NewGallery.java
+++ app/src/main/java/com/example/qpdjg/a2018_seoulapp_owner/NewGallery.java
@@ -1,5 +1,14 @@
 package com.example.qpdjg.a2018_seoulapp_owner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +16,23 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class NewGallery extends AppCompatActivity {
 
@@ -34,16 +54,25 @@ public class NewGallery extends AppCompatActivity {
     String G_location;
     String G_time;
     String G_fee;
+    Button Pic_button;
+    private static final int GALLERY_CODE = 10;
+    private ImageView imageView1;
+    private String imagePath1;
+    private Uri downloadUri1;
 
     private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference mReference = mDatabase.getReference();
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_gallery);
 
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},0);
+        }
+        storage = FirebaseStorage.getInstance();
         Gallery_name = (EditText)findViewById(R.id.Gallery_name);
         Gallery_explain= (EditText)findViewById(R.id.Gallery_explain);
         Owner_explain= (EditText)findViewById(R.id.Owner_explain);
@@ -52,7 +81,8 @@ public class NewGallery extends AppCompatActivity {
         Gallery_time= (EditText)findViewById(R.id.Gallery_time);
         Gallery_fee= (EditText)findViewById(R.id.Gallery_fee);
         submit_button = (Button)findViewById(R.id.submit_button);
-
+        Pic_button = (Button)findViewById(R.id.Pic_Button);
+        imageView1 = (ImageView)findViewById(R.id.imageView1);
 
         G_location_from_list = new String[1];
 
@@ -113,8 +143,89 @@ public class NewGallery extends AppCompatActivity {
         mReference = mDatabase.getReference("OwnerProfile/"+tokenID+"/MyGallerys/"+G_location_from_list[0]);
         mReference.child(G_name).setValue(G_name);
 
+        upload_img(imagePath1);
+
+        /*System.out.println("앙?"+downloadUri1);
+        Gallery_imgs_Data gallery_imgs_data = new Gallery_imgs_Data();
+        gallery_imgs_data.IMG01 = downloadUri1;
+
+        mReference.child(G_name).setValue(gallery_imgs_data);
+*/
         Toast.makeText(getApplicationContext(), "갤러리 등록이 완료 되었습니다. 사용자 어플에서 확인해보세요.", Toast.LENGTH_LONG).show();
 
     }
 
+    public void Pic_pictures(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+
+        startActivityForResult(intent,GALLERY_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == GALLERY_CODE){
+            imagePath1 = getPath(data.getData());
+            File f = new File(imagePath1);
+            imageView1.setImageURI(Uri.fromFile(f));
+
+        }
+    }
+    public String getPath(Uri uri){
+        String [] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader = new CursorLoader(this, uri,proj,null,null,null);
+
+        Cursor cursor = cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(index);
+    }
+
+    private void upload_img(String uri){
+        StorageReference storageRef = storage.getReference();
+
+        Uri file = Uri.fromFile(new File(uri));
+        final StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
+        final UploadTask uploadTask = riversRef.putFile(file);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        // Continue with the task to get the download URL
+                        return riversRef.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            downloadUri1 = task.getResult();
+
+                            mReference = mDatabase.getReference("Gallerys/"+G_location_from_list[0]+"/"+G_name+"/Gallery_imgs");
+
+                            mReference.child("01").setValue(downloadUri1.toString());
+                        } else {
+                            // Handle failures
+                            // ...
+                        }
+                    }
+                });
+            }
+        });
+
+    }
 }
